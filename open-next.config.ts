3e744fae37aaa8f42f58951b038987754978fb85
IMS-SX304 <sxfanpeche@gmail.com>
2025-12-29 11:29:04 +0100

    Create open-next.config.ts

---
+++ open-next.config.ts
@@ -0,0 +1,9 @@
+import type { OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
+  default: {
+    // config minimale, on laisse OpenNext gérer
+  }
+};
+
+export default config;
