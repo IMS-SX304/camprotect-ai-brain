4a0a77e4836d666c880aad85a85199254205e357
IMS-SX304 <sxfanpeche@gmail.com>
2025-12-29 16:07:29 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // IMPORTANT : ton environnement Webflow Cloud est sur /ai
+  basePath: "/ai",
+  assetPrefix: "/ai",
 };
 
 module.exports = nextConfig;
