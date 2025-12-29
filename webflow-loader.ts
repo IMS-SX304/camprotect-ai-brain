274a406e4b00a982e58b350c8c03513a7bb28186
IMS-SX304 <sxfanpeche@gmail.com>
2025-12-29 15:15:30 +0100

    Update webflow-loader.ts

--- webflow-loader.ts
+++ webflow-loader.ts
@@ -1,4 +1,5 @@
-import config from "./webflow-next-config";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require("./webflow-next-config.cjs");
 
 const basePath = config.basePath || "";
 const assetPrefix = config.assetPrefix || basePath;
