809234677e1fec8632e41a10d492a40146b7fd90
IMS-SX304 <sxfanpeche@gmail.com>
2025-12-30 11:01:13 +0100

    Delete scripts directory

--- scripts/rm-next-config-ts.mjs
+++
@@ -1,8 +0,0 @@
-import fs from "node:fs";
-
-if (fs.existsSync("./next.config.ts")) {
-  fs.rmSync("./next.config.ts");
-  console.log("[prebuild] removed next.config.ts");
-} else {
-  console.log("[prebuild] next.config.ts not present");
-}
