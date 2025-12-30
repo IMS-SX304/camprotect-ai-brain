e92ca2d69c6c7a50d4ebcfd64afc48a094de68be
IMS-SX304 <sxfanpeche@gmail.com>
2025-12-30 10:59:16 +0100

    Delete webflow-loader.ts

--- webflow-loader.ts
+++
@@ -1,6 +0,0 @@
-// Webflow Cloud loader shim
-// We avoid importing ./next.config because Webflow may generate next.config.ts
-// or rename files during build, which breaks TS module resolution.
-
-export const basePath = "";
-export const assetPrefix = "";
