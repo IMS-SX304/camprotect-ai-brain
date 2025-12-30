38601e8db8ee1269601009de740d9b4eefa74865
IMS-SX304 <sxfanpeche@gmail.com>
2025-12-30 11:15:03 +0100

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,10 +1,26 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
-
-const config: OpenNextConfig = {
+const config = {
   default: {
     override: {
       wrapper: "cloudflare-node",
-      converter: "edge"
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy"
+    }
+  },
+
+  edgeExternals: ["node:crypto"],
+
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy"
     }
   }
 };
