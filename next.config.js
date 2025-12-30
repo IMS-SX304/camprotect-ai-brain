7194dc8745b84abd607e7853791a43e11ac843e5
IMS-SX304 <sxfanpeche@gmail.com>
2025-12-30 10:35:31 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const mount = process.env.COSMIC_MOUNT_PATH || "";
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: mount,
+  assetPrefix: mount,
 };
 
 module.exports = nextConfig;
